281948c16cee903bc994bda0f9238630d6248dde
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-05-13 13:27:44 +0700

    add:file index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- Header -->
+<?php include './core/header.php'; ?>
+
+
+
+<body class=" d-flex aligns-items-center justify-content-center text-center w-75 position-absolute top-50 start-50 translate-middle">
+  <div>
+    <div class="card" style="width: 20rem;">
+      <img class="card-img-top w-50 mx-auto" src="https://img.icons8.com/ios-filled/500/naruto-sign.png" alt="naruto-sign" />
+      <div class="card-body">
+        <h6 class="card-title">Pendaftaran Shinobi Baru</h6>
+        <h4 class="fw-bold">SD Konoha boarding school</h4>
+        <p class="card-text">List menu : </p>
+        <div class="d-flex gap-2">
+          <a href="./pages/form-daftar.php" class="btn btn-primary">Daftar murid baru</a>
+          <a href="./pages/list-siswa.php" class="btn btn-primary">List yang sudah daftar</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
